05d910448c9e40885fbdf86d500ab9d5b35aed78
Alexander Naumkin <mallanka2byrzoomkin@yandex.ru>
2019-04-25 21:20:47 +0300

    add deconstructor for coordinates and DateTime

--- B-9-Extention-Methods-Deconstruction/Practice.cs
+++ B-9-Extention-Methods-Deconstruction/Practice.cs
@@ -14,10 +14,33 @@ namespace Base_Lesson_9.Practice
         /// Добавить экземплярный метод деконструкции значений координат.
         /// Вывести на консоль длину вектора по координатам.
         /// </summary>
+        public struct Coordinates
+        {
+            private int x, y, z;
+
+            public Coordinates(int x, int y, int z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+
+            public void Deconstruct(out int newX, out int newY, out int newZ, out double length)
+            {
+                newX = x;
+                newY = y;
+                newZ = z;
+                length = Math.Sqrt(newY * newX + newY * newY + newZ * newZ);
+            }
+        }
+
         public static void L9_P_EX_1_from_2()
         {
 
         }
+
+
+
 
         /// <summary>
         /// L9-P-EX-2/2. 
@@ -29,6 +52,19 @@ namespace Base_Lesson_9.Practice
         public static void L9_P_EX_2_from_2()
         {
 
+        }
+    }
+
+    public static class ExtentionDateTime
+    {
+        public static void Deconstruct(this DateTime date, out int year, out int month, out int day, out int hour, out int minute, out int second)
+        {
+            year = DateTime.Now.Year;
+            month = DateTime.Now.Month;
+            day = DateTime.Now.Day;
+            hour = DateTime.Now.Hour;
+            minute = DateTime.Now.Minute;
+            second = DateTime.Now.Second;
         }
     }
 }
--- B-9-Extention-Methods-Deconstruction/Program.cs
+++ B-9-Extention-Methods-Deconstruction/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Base_Lesson_9.Practice;
 
 namespace Base_Lesson_9
 {
@@ -11,8 +12,14 @@ namespace Base_Lesson_9
         static void Main(string[] args)
         {
             //Practice.Practice.L9_P_EX_1_from_2();
+            Practice.Practice.Coordinates coord = new Practice.Practice.Coordinates(3,4,5);
+            var (newX, newY, newZ, length) = coord;
+            Console.WriteLine(length);
             //Practice.Practice.L9_P_EX_2_from_2();
-            Lesson.Lesson.DeconstructExample();
+            DateTime date = new DateTime();
+            var (_, _, _, currentHour, currentMinute, _) = date;
+            Console.WriteLine(currentHour + " " + currentMinute);
+            //Lesson.Lesson.DeconstructExample();
         }
     }
 }
